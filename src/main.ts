48daa2876ab4c3d8f7b91fb821d113a253a0cd54
fractalizes <149290799+fractalizes@users.noreply.github.com>
2025-10-16 05:48:30 +0000

    (step 2 complete) added ability to draw and clear button

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,49 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <h1>Title!</h1>
+  <h1>Doodle Thing!</h1>
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-  <canvas id="canvas"><canvas>
+  <canvas id="canvas"></canvas>
+  <br><br>
+  <button id="clear">clear</button>
 `;
 
+let isDrawing: boolean = false;
+let x: number = 0;
+let y: number = 0;
+
+const clear = document.getElementById("clear")! as HTMLButtonElement;
 const canvas = document.getElementById("canvas")! as HTMLCanvasElement;
 canvas.width = 256;
 canvas.height = 256;
+
+const ctx = canvas.getContext("2d")!;
+
+canvas.addEventListener("mousedown", (mouse) => {
+  x = mouse.offsetX;
+  y = mouse.offsetY;
+
+  isDrawing = true;
+});
+
+canvas.addEventListener("mousemove", (mouse) => {
+  if (isDrawing) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(mouse.offsetX, mouse.offsetY);
+    ctx.stroke();
+
+    x = mouse.offsetX;
+    y = mouse.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", (mouse) => {
+  if (isDrawing) {
+    isDrawing = false;
+  }
+});
+
+clear.addEventListener("mousedown", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
