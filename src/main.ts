ee28f760e07c00d46289c93c28d1ea62aa31aea6
fractalizes <149290799+fractalizes@users.noreply.github.com>
2025-10-16 03:37:13 +0000

    (step 1 complete) added simple canvas with drop shadow and outline

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
+  <h1>Title!</h1>
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <canvas id="canvas"><canvas>
 `;
+
+const canvas = document.getElementById("canvas")! as HTMLCanvasElement;
+canvas.width = 256;
+canvas.height = 256;
